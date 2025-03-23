384fbf3dc287bf7a78a83f1fa51705d8d0088698
Leticia <leticia.delchello@gmail.com>
2025-03-23 20:09:50 -0300

    Criando os jogadores e os inimigos

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,6 @@
 #C
+import pygame
+
 COLOR_PURPLE = (198,130,251)
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0,0,0)
@@ -6,6 +8,7 @@ COLOR_GREEN = (106,211,62)
 COLOR_BLUE = (26,17,96)
 
 #E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -14,6 +17,10 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
 #M
@@ -22,6 +29,22 @@ MENU_OPTION = ('NEW GAME - 1P ',
                'NEW GAME - 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+#P
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                'Player2': pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                   'Player2': pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                   'Player2': pygame.K_LEFT}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
+                    'Player2': pygame.K_RIGHT}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_f,
+                    'Player2': pygame.K_l}
+
+
+#S
+SPAWN_TIME = 4000
 
 #W
 WIN_WIDTH = 576
