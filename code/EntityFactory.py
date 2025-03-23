aaa357a07089363e32242bf179ba8d5ce955df20
Leticia <leticia.delchello@gmail.com>
2025-03-23 16:35:04 -0300

    Alterações dia 0323

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,6 @@
 from code.Background import Background
 from code.Const import WIN_WIDTH
+from code.Player import Player
 
 
 class EntityFactory:
@@ -13,4 +14,6 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, WIN_WIDTH / 2))
 
